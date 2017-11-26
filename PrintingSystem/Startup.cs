459bcdde9822003cb550dc2e1b7e154667d22932
Abu Zafor Fagun <mdabuzaforfagun@gmail.com>
2017-11-26 09:11:18 +0600

    Initial MVC Project

---
+++ PrintingSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrintingSystem.Startup))]
+namespace PrintingSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
